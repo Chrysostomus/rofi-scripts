99e421a4b17fa6b115eae897c149a3c190ffb856
Matti Hyttinen <mattitheman@gmail.com>
2019-03-28 13:27:12 +0200

    Update rofi-finder.sh

--- bin/rofi-finder.sh
+++ bin/rofi-finder.sh
@@ -1,11 +1,20 @@
-#!/bin/zsh
+#!/bin/dash
 # A fuzzy file-finder and opener based on rofi 
 # Requires: rofi, xdg-utils, nerdfonts
+files=/tmp/.file_list
 
-files=$(find -type d -not -path '*/\.*' 2> /dev/null | sed -e 's/^\./:~/g'
-find -not -type d 2> /dev/null | sed -E -e '/png$|jpg$|tiff$|gif$|jpeg$|bmp$/ s/^\./:~/g' -e '/mp3$|pcm$|wav$|aac$|ogg$|wma$/ s/^\./~/g' -e '/wmv$|webm$|m4v$|mkv$|mov$|flv$|avi$|mp4$/ s/^\./辶:~/g' -e 's/^\./:~/g')
-
-input=$(echo $files | rofi -dmenu -p :) 
+if [ -e $files ]; then
+	# File list exist, use it
+	input=$(cat $files | rofi -dmenu -p )
+	# Update the list on the background
+	find -type d -not -path '*/\.*' 2> /dev/null | sed -e 's/^\./:~/g' > /tmp/.file_list &
+	find -not -type d 2> /dev/null | sed -E -e '/png$|jpg$|tiff$|gif$|jpeg$|bmp$/ s/^\./:~/g' -e '/mp3$|pcm$|wav$|aac$|ogg$|wma$/ s/^\./~/g' -e '/wmv$|webm$|m4v$|mkv$|mov$|flv$|avi$|mp4$/ s/^\./辶:~/g' -e 's/^\./:~/g' >> /tmp/.file_list & 
+else
+	# There is no file list, create it and show menu only after that
+	find -type d -not -path '*/\.*' 2> /dev/null | sed -e 's/^\./:~/g' > /tmp/.file_list
+	find -not -type d 2> /dev/null | sed -E -e '/png$|jpg$|tiff$|gif$|jpeg$|bmp$/ s/^\./:~/g' -e '/mp3$|pcm$|wav$|aac$|ogg$|wma$/ s/^\./~/g' -e '/wmv$|webm$|m4v$|mkv$|mov$|flv$|avi$|mp4$/ s/^\./辶:~/g' -e 's/^\./:~/g' >> /tmp/.file_list
+	input=$(cat $files | rofi -dmenu -p )
+fi
 	case "$(echo $input | cut -d " " -f 1)" in
 		a)
 			exec $BROWSER https://wiki.archlinux.org/index.php/"$(echo $input | cut -d " " -f2-)" &> /dev/null &
